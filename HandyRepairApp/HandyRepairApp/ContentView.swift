d610128c44c6db6d9287f662583cb293c1b4ecc4
Nicolá Domingues <nico.domingues.dev@gmail.com>
2025-01-22 23:48:58 +0000

    initial commit

---
+++ HandyRepairApp/HandyRepairApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HandyRepairApp
+//
+//  Created by Nicolá Domingues on 22/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HandyRepairApp/HandyRepairApp/HRApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRApp.swift
+//  HandyRepairApp
+//
+//  Created by Nicolá Domingues on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
